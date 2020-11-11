2ded5b8d10d9f6c76e9b9b1a80ffde8533005d10
DaThundA <70856272+DaThundA@users.noreply.github.com>
2020-11-11 15:30:45 -0600

    Create TilePanel.java

---
+++ Taylor_Gavin_cpsc24500/TilePanel.java
@@ -0,0 +1,11 @@
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
+import javax.swing.JPanel;
+public class TilePanel extends JPanel Implements MouseListener, MouseMoationListener {
+	private String MouseStatus;
+	private ArrayList<Shape> shapes;
+	
+}

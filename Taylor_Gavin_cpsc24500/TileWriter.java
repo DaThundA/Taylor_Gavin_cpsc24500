ca17cced7bcb3e8f9cda44b88255d9cc1a40f865
DaThundA <70856272+DaThundA@users.noreply.github.com>
2020-11-16 10:54:59 -0600

    Update TileWriter.java
    Added comments and the ability to write XML files.

--- Taylor_Gavin_cpsc24500/TileWriter.java
+++ Taylor_Gavin_cpsc24500/TileWriter.java
@@ -1,4 +1,3 @@
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
@@ -6,7 +5,14 @@ import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
+
 public class TileWriter {
+	/**
+	 * These two functions deal with converting the data into a text file.
+	 * @param fname
+	 * @param tiles
+	 * @return
+	 */
 	public boolean writeToText(String fname, ArrayList<Tile> tiles) {
 		File f = new File(fname);
 		return writeToText(f, tiles);
@@ -24,13 +30,19 @@ public class TileWriter {
 			return false;
 		}
 	}
+	/**
+	 * These two functions takes the data and turns it into a binary file.
+	 * @param fname
+	 * @param tiles
+	 * @return
+	 */
 	public boolean writeToBinary(String fname, ArrayList<Tile> tiles) {
 		File f = new File(fname);
 		return writeToBinary(f, tiles);
 	}
-	public boolean writeToBinary(File f, Arraylist<Tile> tiles) {
+	public boolean writeToBinary(File f, ArrayList<Tile> tiles) {
 		try {
-			ObjectOutputStream oos = new ObjectOutputStream(new FileOutpviutStream(f));
+			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f));
 			oos.writeObject(tiles);
 			oos.close();
 			return true;
@@ -38,4 +50,50 @@ public class TileWriter {
 			return false;
 		}
 	}
+	/**
+	 * These functions are used to convert data in to a XML file.
+	 * @param fname
+	 * @param tiles
+	 * @return
+	 */
+	public boolean writeToXML(String fname, ArrayList<Tile> tiles) {
+		File f = new File(fname);
+		return writeToXML(f, tiles);
+	}
+	public boolean writeToXML(File f, ArrayList<Tile> tiles) {
+		try {
+			XMLEncoder enc = new XMLEncoder(new BufferedOutputStream(new FileOutputStream(f)));
+			enc.writeObject(tiles);
+			enc.close();
+			return true;
+		}catch (Exception ex) {
+			return false;
+		}
+	}
+	public boolean write(String fname, ArrayList<Tile> tiles) {
+		File f = new File(fname);
+		return write(f, tiles);
+	}
+	/**
+	 * After hitting the save button and giving the file a name
+	 * whatever is at the end "TXT, BIN, XML" then it will turn
+	 * that save into the corisponding data.
+	 * @param f
+	 * @param tiles
+	 * @return
+	 */
+	public boolean write(File f, ArrayList<Tile> tiles) {
+		String fname = f.getName().toUpperCase();
+		if (fname.endsWith(".TXT")) {
+			return writeToText(f, tiles);
+		}
+		if (fname.endsWith(".BIN")) {
+			return writeToBinary(f, tiles);
+		}
+		if (fname.endsWith(".XML")) {
+			return writeToXML(f, tiles);
+		}
+		return false;
+	}
 }
+

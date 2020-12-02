6390aaaa560b75e06bab90c459a97bebe47ba91f
DaThundA <70856272+DaThundA@users.noreply.github.com>
2020-12-02 17:04:25 -0600

    Fixed bugs

--- Taylor_Gavin_cpsc24500/Tile.java
+++ Taylor_Gavin_cpsc24500/Tile.java
@@ -1,7 +1,6 @@
+import java.awt.Color;
 import java.io.Serializable;
 import java.util.Random;
-import java.util.ArrayList;
-import java.awt.Point;
 /**
  * In this class the varibles for the color and shape are determined and sent to TilePanel.java
  * This also set varibles to posX and posY for the location of each tile.
@@ -9,50 +8,68 @@ import java.awt.Point;
  *
  */
 public class Tile implements Serializable {
-	Random rand = new Random();
+	//The the first three private varibles are constants and can only be accessed in this class
+	private static final Color[] colors = {Color.YELLOW, Color.GREEN, Color.ORANGE, Color.RED, Color.BLUE};
+	private static final String[] colorNames = {"yellow", "green", "orange", "red", "blue"};
+	private static final String[] shapes = {"circle", "square"};
 	private int color;
 	private int shape;
-	private int posX;
-	private int posY;
 	public int getColor() {
 		return color;
 	}
 	public void setColor(int color) {
-		color = rand.nextInt(2);//This picks a random number 0 or 1
-		this.color = color;
+		if (color < 0) {
+			this.color = 0;
+		}else if (color > 4){
+			this.color = 4;
+		}else {
+			this.color = color;
+		}
 	}
 	public int getShape() {
 		return shape;
 	}
 	public void setShape(int shape) {
-		shape = rand.nextInt(5);//this picks a random number between 0 and 4
-		this.shape = shape;
+		if (shape < 0) {
+			this.shape = 0;
+		}else if (shape > 1) {
+			this.shape = 1;
+		}else {
+			this.shape = shape;
+		}
 	}
-	public int getPosX() {
-		return posX;
+	//Here this function randomizes the color and shape from a the arraylist
+	//and assigns a random color and shape accordingly 
+	public void setRandomly(Random rn) {
+		color = rn.nextInt(colors.length);
+		shape = rn.nextInt(shapes.length);
 	}
-	public void setPosX(int posX) {
-		this.posX = posX;
+	public Color getRealColor() {
+		return colors[color];
 	}
-	public int getPosY() {
-		return posY;
+	public String getColorName() {
+		return colorNames[color];
 	}
-	public void setPosY(int posY) {
-		this.posY = posY; 
+	public String getShapeName() {
+		return shapes[shape];
 	}
 	public Tile() {
-		color = 1;
-		shape = 1;
-		posX = 1;
-		posY = 1;
+		color = 3;
+		shape = 0;
 	}
-	public Tile(int color, int shape, int posX, int posY) {//This is all the data that is in the Tile
+	public Tile(int color, int shape) {//This is all the data that is in the Tile
 		setColor(color);
 		setShape(shape);
-		setPosX(posX);
-		setPosY(posY);
+	}
+	//All three of these are used when the data is saved eith to and XML file,
+	//BIN file or TXT file
+	public String toStringNice(){
+		return String.format("%s %s", getColorName(), getShapeName());
 	}
 	public String toString() {
-		return String.format("%d %d %d %d", color, shape, posX, posY);//This is what will be read will be read
+		return String.format("%d %d", color, shape);//This is what will be read will be read
+	}
+	public int getCode() {
+		return color*10+shape;
 	}
 }
